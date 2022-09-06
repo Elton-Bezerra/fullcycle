92b7cb4acc8856d450092d1b14adb92295d88f1e
Elton Bezerra <elton.cin@hotmail.com>
2022-09-05 22:20:13 -0300

    Adding grpc client

---
+++ grpc/cmd/client/client.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"log"
+	"os"
+
+	"github.com/Elton-Bezerra/fullcycle/pb"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials/insecure"
+)
+
+func main() {
+	connection, err := grpc.Dial("localhost:50051", grpc.WithTransportCredentials(insecure.NewCredentials()))
+	if err != nil {
+		log.Fatalf("Could not connect to gRPC Server: %v", err)
+	}
+
+	defer connection.Close()
+
+	client := pb.NewUserServiceClient(connection)
+
+	AddUser(client)
+}
+
+func AddUser(client pb.UserServiceClient) {
+	req := &pb.User{
+		Id:    "0",
+		Name:  "João",
+		Email: "j@j.com",
+	}
+
+	res, err := client.AddUser(context.Background(), req)
+	if err != nil {
+		log.Fatalf("Could not connect make gRPC request: %v", err)
+	}
+
+	fmt.Fprintln(os.Stdout, res)
+}
